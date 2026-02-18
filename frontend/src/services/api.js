d433b9577310639101ca92a4a2bde4efb27c1935
tusharv <tusharv@mkcl.org>
2026-02-18 12:41:53 +0530

    Configure GitHub Pages deployment and update API IP

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://10.2.10.76:8080/api',
     headers: {
         'Content-Type': 'application/json',
     },
